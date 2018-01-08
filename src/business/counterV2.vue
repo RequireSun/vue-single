0e2a07dd62b10b6488c545028da505fd0776f72c
RequireSun <kelvinsen@outlook.com>
2018-01-08 23:17:59 +0800

    new usage

--- src/business/counterV2.vue
+++ src/business/counterV2.vue
@@ -1,69 +1,18 @@
 <template>
   <div>
-    <div>
-      <h1>{{state}}</h1>
-      <span>{{state.count}}</span>
-      <!--<div>{{stores.actions.set}}</div>-->
-      <button @click="clear">actions in methods (clear)</button>
-      <button @click="stores.actions.set({count:77})">actions on element (set)</button>
-      <button @click="setAfter1">business in methods (set after 1 second)</button>
-    </div>
+    <h1>{{state}}</h1>
+    <span>{{state.count}}</span>
+    <button @click="clear">actions in methods (clear)</button>
+    <button @click="stores.actions.set({count:77})">actions on element (set)</button>
+    <button @click="setAfter1">business in methods (set after 1 second)</button>
   </div>
 </template>
 
 <script>
-  import connect from 'vue-own-redux';
+  import decorator from 'vue-own-redux/decorator';
 
-  function connectDecorator(target) {
-    function onStateChange() {
-      this.state = this.stores.store.getState();
-    }
-
-    const data = target.prototype.data;
-    const mounted = target.prototype.mounted;
-    const beforeDestroy = target.prototype.beforeDestroy;
-
-    target.prototype.data = function () {
-      return Object.assign(('function' === typeof data && data()) || {}, {
-        state: this.stores && this.stores.store ? this.stores.store.getState() : {}
-      });
-    };
-
-    target.prototype.mounted = function () {
-      if (this.stores && this.stores.store && this.stores.store.subscribe) {
-        this.unsubscribe = this.stores.store.subscribe(onStateChange.bind(this));
-        onStateChange.call(this);
-      }
-
-      this.$watch('stores', function (val, oldVal) {
-        if (val !== oldVal) {
-          this.unsubscribe && this.unsubscribe();
-          this.unsubscribe = undefined;
-          if (val && val.store && val.store.subscribe) {
-            this.unsubscribe = val.store.subscribe(onStateChange.bind(this));
-            onStateChange.call(this);
-          }
-        }
-      });
-
-      'function' === typeof mounted && mounted.call(this);
-    };
-
-    target.prototype.beforeDestroy = function () {
-      this.unsubscribe && this.unsubscribe();
-      this.unsubscribe = undefined;
-
-      'function' === typeof beforeDestroy && beforeDestroy.call(this);
-    };
-
-  }
-
-  @connectDecorator
+  @decorator
   class Main {
-    components = {
-      connect,
-    };
-
     props = ['stores'];
 
     methods = {
